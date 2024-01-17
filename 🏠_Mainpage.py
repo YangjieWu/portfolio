6bb980d7814fe5eeb163ca80c1b659ede0e9e9f4
YangjieWu <98076128+YangjieWu@users.noreply.github.com>
2024-01-17 06:41:06 +0000

    first_edit_after_fork

--- 🏠_Mainpage.py
+++ 🏠_Mainpage.py
@@ -18,20 +18,27 @@ col1, col2, col3 = st.columns([1.3 ,0.2, 1])
 
 with col1:
     st.write(
-    ' Starting from physical therapist, to UX designer, to software engineer and product manager... ' +
-    'I am a passionate and curious explorer currently pursuing a Computer Science and Anthropology major at Brandeis University, expected to graduate in May 2025. ' +
-    '**I believe in the intersectionality of quantitative and qualitative subjects, that neither approach alone can lead one to the absolute truth.**'
+    ' Hello everyone!\
+        My name is Yangjie and I hail from the bustling city of Shanghai, China.\
+        I am a data scientist. My expertise lies in machine learning, statistics, and data driven business consulting.\
+        I have spent my student life in Canada and have had the opportunity to experience Japanese culture extensively, both in Kanto and Kansai.'
+    +
+    'When I am not crunching numbers and creating algorithms,\
+        you can find me hiking in the forest, learning French, or cooking up a storm in my kitchen.\
+        I also enjoy staying active by playing tennis and badminton in my local community.'
+    +
+    '**I believe the First Principle Theory **'
     )
-    st.markdown("###### 😄 Name: Haoran Cheng, just call me Harry!")
-    st.markdown("###### 👉 Study: Brandeis University")
+    st.markdown("###### 😄 Nick Name: William!")
+    st.markdown("###### 👉 Study: UBC")
     # st.markdown("#### 🇨🇳 Nationality: Chinese")
-    st.markdown("###### 📍 Location: Boston, MA")
+    st.markdown("###### 📍 Location: Osaka, JP")
     st.markdown("###### 🏄 Interest: Full Stack, Data Science, Product Management")
-    st.markdown("###### 🟡 Favorite Color: Yellow")
-    st.markdown("###### 👀 Linkedin: www.linkedin.com/in/haoran-cheng-018b9318b/")
+    st.markdown("###### 🟡 Favorite Color: Organge")
+    st.markdown("###### 👀 Linkedin: https://www.linkedin.com/in/yangjiewu/")
     
     #st.divider()
-    with open("src/resume.pdf", "rb") as file:
+    with open("src/CV_YangjieWu_20231208.pdf", "rb") as file:
         pdf_file = file.read()
 
     st.download_button(
